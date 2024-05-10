555bc61796043b0fbe67f681f60a1269c66eeae1
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-05-10 02:24:26 +0000

    1

--- app.rb
+++ app.rb
@@ -7,3 +7,22 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock")do
+selection = ["rock","paper","scissors"]
+computer_selection = selection.sample
+
+if computer_selection == "rock"
+  outcome = "Tied"
+elsif computer_selection == "scissors"
+  outcome = "You Win!"
+else  computer_selection == "paper"
+  outcome = "You Lose!"
+end
+"
+<h2> We played rock <h2>
+<h2> They played  #{computer_selection} <h2>
+<h2>  #{outcome}<h2>
+"
+
+end
